c74d8a50ef16b71480eea9e9acae462f6fd71fc6
Nathan Zhang <nzhang32@gmail.com>
2023-07-17 18:59:30 -0700

    Added log caching
    This means that once we get a log,
    it doesn't need to be re-fetched.
    
    This results in a ~50% speedup on basic tests.

--- dam-core/src/metric.rs
+++ dam-core/src/metric.rs
@@ -1,22 +1,39 @@
+use std::{cell::RefCell, collections::HashMap};
+
 use linkme::distributed_slice;
 
-use crate::log_graph;
+use crate::log_graph::{self, WeakEventLogger};
+
+thread_local! {
+    static LOG_CACHE: RefCell<HashMap<&'static str, WeakEventLogger>> = RefCell::new(HashMap::new());
+}
 
 pub trait LogProducer {
     // This should be a simple name consisting only of [a-zA-Z] and "-" characters.
     const LOG_NAME: &'static str;
 
     fn log<T: serde::Serialize + std::fmt::Debug>(value: T) {
-        let thread_id = std::thread::current().id();
-        let current_graph = crate::log_graph::get_graph();
-        let identifier = current_graph.get_identifier(thread_id);
-        current_graph
-            .get_log(log_graph::LogType::Event(
-                identifier,
-                thread_id,
-                Self::LOG_NAME,
-            ))
-            .log(value);
+        LOG_CACHE.with(|cache: &RefCell<HashMap<&str, WeakEventLogger>>| {
+            if !cache.borrow().contains_key(Self::LOG_NAME) {
+                let thread_id = std::thread::current().id();
+                let current_graph = crate::log_graph::get_graph();
+                let identifier = current_graph.get_identifier(thread_id);
+                let log = current_graph.get_log(log_graph::LogType::Event(
+                    identifier,
+                    thread_id,
+                    Self::LOG_NAME,
+                ));
+                cache.borrow_mut().insert(Self::LOG_NAME, log.weak());
+            }
+
+            cache
+                .borrow()
+                .get(Self::LOG_NAME)
+                .expect("We should have gotten a log!")
+                .promote()
+                .expect("We're not dead yet, why is our log?")
+                .log(value);
+        });
     }
 }
 
