5c4c78402394159ab98e8c9df6d369aa9f92122e
Gina Sohn <gina7484@gmail.com>
2023-07-27 19:09:22 -0700

    qkt node

--- src/templates/streamingattn/stream_qkt.rs
+++ src/templates/streamingattn/stream_qkt.rs
@@ -3,7 +3,7 @@ use dam_core::identifier::Identifier;
 use dam_core::TimeManager;
 use dam_macros::{cleanup, identifiable, time_managed};
 
-use ndarray::{ArrayBase, Dim, OwnedRepr};
+use ndarray::{ArrayBase, Axis, Dimension, OwnedRepr};
 
 use crate::{
     channel::{
@@ -35,6 +35,7 @@ where
 #[identifiable]
 pub struct QKT<A: Clone, D: Clone> {
     qkt_data: QKTData<A, D>,
+    //latency: dam_core::time::Time,
 }
 
 impl<A: Clone, D: Clone> QKT<A, D>
@@ -45,6 +46,7 @@ where
     pub fn new(qkt_data: QKTData<A, D>) -> Self {
         let qkt = QKT {
             qkt_data,
+            // latency,
             time: TimeManager::default(),
             identifier: Identifier::new(),
         };
@@ -56,17 +58,64 @@ where
     }
 }
 
-impl<A: Clone, D: Clone> Context for QKT<A, D>
+impl<A, D> Context for QKT<A, D>
 where
-    ArrayBase<OwnedRepr<A>, D>: DAMType,
+    ArrayBase<OwnedRepr<A>, D>: DAMType + std::ops::Mul<Output = ArrayBase<OwnedRepr<A>, D>>,
+    D: Dimension<Smaller = D> + Clone + ndarray::RemoveAxis,
+    A: Clone + num::Num + std::iter::Sum,
 {
     fn init(&mut self) {}
 
-    fn run(&mut self) -> () {}
+    fn run(&mut self) -> () {
+        loop {
+            let in1_deq = peek_next(&mut self.time, &mut self.qkt_data.in1);
+            let in2_deq = peek_next(&mut self.time, &mut self.qkt_data.in2);
+
+            match (in1_deq, in2_deq) {
+                (Ok(in1), Ok(in2)) => {
+                    let mul_in12: ArrayBase<OwnedRepr<A>, D> = in1.data.clone() * in2.data;
+                    // we clone as the multiplication consumes the first operand
+                    let reduce_sum =
+                        mul_in12.map_axis(Axis(0), |view| view.to_owned().into_iter().sum::<A>());
+                    let curr_time = self.time.tick();
+                    enqueue(
+                        &mut self.time,
+                        &mut self.qkt_data.out,
+                        ChannelElement::new(curr_time + 5, reduce_sum),
+                        // assuming a pipeline depth = 5 (this could be later parameterized through latency)
+                    )
+                    .unwrap();
+                    dequeue(&mut self.time, &mut self.qkt_data.in1).unwrap();
+                    dequeue(&mut self.time, &mut self.qkt_data.in2).unwrap();
+                }
+                (_, _) => {
+                    panic!("Reached unhandled case");
+                }
+            }
+            self.time.incr_cycles(1); // initiation interval (could also be parameterized later)
+        }
+    }
 
     #[cleanup(time_managed)]
     fn cleanup(&mut self) {
         self.qkt_data.cleanup();
         self.time.cleanup();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use ndarray::{array, ArrayBase, Axis, Dim, OwnedRepr};
+
+    #[test]
+    fn test_ndarray_clone() {
+        let array1 = array![[1, 2, 1], [3, 4, 5]];
+        let array2 = array![[1, 2, 1], [3, 4, 5]];
+        let array3 = array1.clone() * array2;
+        //println!("{:?}", array1);
+        println!("{:?}", array3);
+        let reduction_arr3 = array3.map_axis(Axis(0), |view| view.iter().sum::<i32>());
+        println!("{:?}", reduction_arr3);
     }
 }
