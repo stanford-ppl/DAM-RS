f3a288af333b3079a1cd20c0fb5ed849e4888e41
Gina Sohn <gina7484@gmail.com>
2023-07-28 20:54:08 -0700

    Add rowsum, fix latency bug for max, test pass with FIFO depth 2

--- src/templates/streamingattn/mod.rs
+++ src/templates/streamingattn/mod.rs
@@ -1,3 +1,3 @@
 pub mod stream_qkt;
 pub mod stream_qkt_out2;
-pub mod stream_rowmax;
+pub mod stream_reduce;
--- src/templates/streamingattn/stream_reduce.rs
+++ src/templates/streamingattn/stream_reduce.rs
@@ -16,7 +16,7 @@ use crate::{
 pub struct ReduceData<A: Clone> {
     pub in_stream: Receiver<ArrayBase<OwnedRepr<A>, Dim<[usize; 2]>>>,
     pub out_stream: Sender<ArrayBase<OwnedRepr<A>, Dim<[usize; 2]>>>,
-    pub latency: u64,       // pipeline depth
+    //pub latency: u64,       // pipeline depth -> We assume a single cycle
     pub init_inverval: u64, // initiation interval
     pub seq_len: u64,
 }
@@ -106,7 +106,87 @@ where
                     enqueue(
                         &mut self.time,
                         &mut self.reduce_data.out_stream,
-                        ChannelElement::new(curr_time + self.reduce_data.latency, temp_res),
+                        ChannelElement::new(curr_time, temp_res),
+                    )
+                    .unwrap();
+                }
+                _ => {
+                    panic!("Reached unhandled case");
+                }
+            }
+        }
+    }
+
+    #[cleanup(time_managed)]
+    fn cleanup(&mut self) {
+        self.reduce_data.cleanup();
+        self.time.cleanup();
+    }
+}
+
+#[time_managed]
+#[identifiable]
+pub struct RowSum<A: Clone> {
+    reduce_data: ReduceData<A>,
+}
+
+impl<A: DAMType> RowSum<A>
+where
+    RowSum<A>: Context,
+    ArrayBase<OwnedRepr<A>, Dim<[usize; 2]>>: DAMType,
+{
+    pub fn new(reduce_data: ReduceData<A>) -> Self {
+        let reduce = RowSum {
+            reduce_data,
+            time: TimeManager::default(),
+            identifier: Identifier::new(),
+        };
+        (reduce.reduce_data.in_stream).attach_receiver(&reduce);
+        (reduce.reduce_data.out_stream).attach_sender(&reduce);
+
+        reduce
+    }
+}
+
+impl<A> Context for RowSum<A>
+where
+    ArrayBase<OwnedRepr<A>, Dim<[usize; 2]>>: DAMType
+        //+ std::ops::Mul<Output = ArrayBase<OwnedRepr<A>, Dim<[usize; 2]>>>
+        + ndarray::linalg::Dot<
+            ArrayBase<OwnedRepr<A>, Dim<[usize; 2]>>,
+            Output = ArrayBase<OwnedRepr<A>, Dim<[usize; 2]>>,
+        >,
+    A: DAMType + num::Num + std::cmp::PartialOrd + Copy,
+{
+    fn init(&mut self) {}
+
+    fn run(&mut self) -> () {
+        for _i in 0..self.reduce_data.seq_len {
+            // outer loop
+            let first_peek = dequeue(&mut self.time, &mut self.reduce_data.in_stream);
+            match first_peek {
+                Ok(first_elem) => {
+                    let mut temp_res = first_elem.data;
+                    self.time.incr_cycles(self.reduce_data.init_inverval);
+                    for _i in 1..self.reduce_data.seq_len {
+                        // inner loop
+                        let in_deq = dequeue(&mut self.time, &mut self.reduce_data.in_stream);
+                        match in_deq {
+                            Ok(in_elem) => {
+                                let new_temp = &temp_res + &in_elem.data;
+                                temp_res = new_temp;
+                                self.time.incr_cycles(self.reduce_data.init_inverval);
+                            }
+                            _ => {
+                                panic!("Reached unhandled case");
+                            }
+                        }
+                    }
+                    let curr_time = self.time.tick();
+                    enqueue(
+                        &mut self.time,
+                        &mut self.reduce_data.out_stream,
+                        ChannelElement::new(curr_time, temp_res),
                     )
                     .unwrap();
                 }
@@ -132,7 +212,7 @@ mod tests {
     };
     use ndarray::{array, Array, ArrayBase, Dim, OwnedRepr};
 
-    use super::{ReduceData, RowMax};
+    use super::{ReduceData, RowMax, RowSum};
 
     #[test]
     fn test_reduce_array() {
@@ -157,7 +237,7 @@ mod tests {
     }
 
     #[test]
-    fn stream_reduce_test() {
+    fn stream_reduce_max_test() {
         const HEAD_DIM: usize = 16;
         const SEQ_LEN: u64 = 5;
         const LATENCY: u64 = SEQ_LEN;
@@ -168,7 +248,7 @@ mod tests {
         const HEAD_DIM_I32: i32 = 16;
 
         // We will use seq length of 5 for now. So, conservatively keep the FIFO (Channel) size as 5.
-        let chan_size = 30; // FIFO Depth
+        let chan_size = 2; // FIFO Depth
 
         let mut parent = Program::default();
 
@@ -182,12 +262,12 @@ mod tests {
         let data = ReduceData::<i32> {
             in_stream: in1_receiver,
             out_stream: out_sender,
-            latency: LATENCY,
+            //latency: LATENCY,
             init_inverval: INIT_INTERVAL,
             seq_len: SEQ_LEN,
         };
 
-        let stream_reduce = RowMax::new(data);
+        let stream_reduce_max = RowMax::new(data);
 
         // Create the Iterators for Generators
         let in1_iter = || (0..(SEQ_LEN_I32 * SEQ_LEN_I32)).map(|i| array![[i]]);
@@ -200,7 +280,56 @@ mod tests {
 
         parent.add_child(gen1);
         parent.add_child(stream_reduce_checker);
-        parent.add_child(stream_reduce);
+        parent.add_child(stream_reduce_max);
+        parent.init();
+        parent.run();
+    }
+
+    #[test]
+    fn stream_reduce_sum_test() {
+        const HEAD_DIM: usize = 16;
+        const SEQ_LEN: u64 = 5;
+        const LATENCY: u64 = SEQ_LEN;
+        const INIT_INTERVAL: u64 = 1;
+
+        // I32 types for generating data
+        const SEQ_LEN_I32: i32 = 5;
+        const HEAD_DIM_I32: i32 = 16;
+
+        // We will use seq length of 5 for now. So, conservatively keep the FIFO (Channel) size as 5.
+        let chan_size = 2; // FIFO Depth
+
+        let mut parent = Program::default();
+
+        // Create Channels
+        let (in1_sender, in1_receiver) =
+            parent.bounded::<ArrayBase<OwnedRepr<i32>, Dim<[usize; 2]>>>(chan_size);
+        let (out_sender, out_receiver) =
+            parent.bounded::<ArrayBase<OwnedRepr<i32>, Dim<[usize; 2]>>>(chan_size);
+
+        // Create the Reduce block
+        let data = ReduceData::<i32> {
+            in_stream: in1_receiver,
+            out_stream: out_sender,
+            //latency: LATENCY,
+            init_inverval: INIT_INTERVAL,
+            seq_len: SEQ_LEN,
+        };
+
+        let stream_reduce_sum = RowSum::new(data);
+
+        // Create the Iterators for Generators
+        let in1_iter = || (0..(SEQ_LEN_I32 * SEQ_LEN_I32)).map(|_i| array![[1]]);
+
+        // Create the Iterators for Checkers
+        let out_iter = || (0..(SEQ_LEN_I32)).map(|_i| array![[SEQ_LEN_I32]]);
+
+        let gen1 = GeneratorContext::new(in1_iter, in1_sender); // Q : [1,D] shaped vectors
+        let stream_reduce_checker = CheckerContext::new(out_iter, out_receiver);
+
+        parent.add_child(gen1);
+        parent.add_child(stream_reduce_checker);
+        parent.add_child(stream_reduce_sum);
         parent.init();
         parent.run();
     }
