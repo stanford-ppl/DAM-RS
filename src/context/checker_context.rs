51f8b6ef59022d4a41b114fba2305175dcaa113d
Gina Sohn <gina7484@gmail.com>
2023-08-01 14:52:59 -0700

    Support bounded checkers for scalar fp

--- src/context/checker_context.rs
+++ src/context/checker_context.rs
@@ -64,3 +64,70 @@ where
         gc
     }
 }
+
+#[time_managed]
+#[identifiable]
+pub struct FpBoundedCheckerContext<T: Clone, IType, FType>
+where
+    IType: Iterator<Item = T>,
+    FType: FnOnce() -> IType + Send + Sync,
+{
+    iterator: Option<FType>,
+    input: Receiver<T>,
+    bound: T,
+}
+
+impl<T: DAMType, IType, FType> Context for FpBoundedCheckerContext<T, IType, FType>
+where
+    T: num::Float,
+    IType: Iterator<Item = T>,
+    FType: FnOnce() -> IType + Send + Sync,
+{
+    fn init(&mut self) {}
+
+    fn run(&mut self) {
+        if let Some(iter) = self.iterator.take() {
+            for (ind, val) in iter().enumerate() {
+                match dequeue(&mut self.time, &mut self.input) {
+                    Ok(ChannelElement { time, data })
+                        if (data - val) > self.bound || (data - val).abs() > self.bound =>
+                    {
+                        panic!("Mismatch on iteration {ind} at time {time:?}: Expected {val:?} but found {data:?}")
+                    }
+                    Ok(_) => {}
+                    Err(_) => {
+                        panic!("Ran out of things to read on iteration {ind}, expected {val:?}")
+                    }
+                }
+            }
+        } else {
+            panic!("Cannot run a Checker twice!");
+        }
+    }
+
+    #[cleanup(time_managed)]
+    fn cleanup(&mut self) {}
+}
+
+impl<T: DAMType, IType, FType> FpBoundedCheckerContext<T, IType, FType>
+where
+    T: num::Float,
+    IType: Iterator<Item = T>,
+    FType: FnOnce() -> IType + Send + Sync,
+{
+    pub fn new(
+        iterator: FType,
+        input: Receiver<T>,
+        bound: T,
+    ) -> FpBoundedCheckerContext<T, IType, FType> {
+        let gc = FpBoundedCheckerContext {
+            iterator: Some(iterator),
+            input,
+            bound,
+            time: Default::default(),
+            identifier: Identifier::new(),
+        };
+        gc.input.attach_receiver(&gc);
+        gc
+    }
+}
