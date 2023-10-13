56dee5e37066331221041c7dea304731ea5992e8
Gina Sohn <gina7484@gmail.com>
2023-10-13 12:50:48 -0700

    updated test for vector DAMType impl

--- src/types/vector.rs
+++ src/types/vector.rs
@@ -11,27 +11,15 @@ where
 
 #[cfg(test)]
 mod tests {
-    use std::ops::Mul;
-
-    use ndarray::array;
-
     use crate::types::{DAMType, StaticallySized};
 
     #[test]
     fn test_ndarray() {
-        let mat_a = array![[1, 2], [3, 4]];
-        let mat_b = array![[5, 6], [7, 8]];
+        let vec_a: Vec<i32> = vec![1, 2, 3];
 
-        let ref_size = i32::SIZE * 4;
+        let ref_size = i32::SIZE * 3;
 
-        assert!(mat_a.dam_size() == ref_size);
-        assert!(mat_b.dam_size() == ref_size);
-
-        let mat_c = (&mat_a).mul(&mat_b);
-        dbg!(mat_c);
-
-        let mat_d = mat_a + mat_b;
-
-        dbg!(mat_d);
+        assert!(vec_a.dam_size() == ref_size);
+        dbg!(vec_a);
     }
 }
